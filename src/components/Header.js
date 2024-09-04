ec235a23a4a3aa142e1145f53600990aa565455b
Yashwanth <yashyash517@gmail.com>
2024-09-04 17:25:51 +0530

    secondcommit

--- src/components/Header.js
+++ src/components/Header.js
@@ -51,7 +51,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header style={styles.header}>
-      <div style={styles.logo}>😄😔😭💀🥹</div>
+      <div style={styles.logo}>💀💀</div>
       <nav>
         <ul style={styles.navList}>
           <li><Link to="/" style={styles.navItem}>Home</Link></li>
